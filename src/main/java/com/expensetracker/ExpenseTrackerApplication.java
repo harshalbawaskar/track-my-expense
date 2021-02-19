ef9f5c766ca2e306e43c45c84dd5b086dbf3e161
harshalbawaskar <harshalbawaskar>
2021-02-20 00:56:42 +0530

    add cors support

--- src/main/java/com/expensetracker/ExpenseTrackerApplication.java
+++ src/main/java/com/expensetracker/ExpenseTrackerApplication.java
@@ -5,12 +5,34 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 public class ExpenseTrackerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ExpenseTrackerApplication.class, args);
+	}
+
+	@Bean
+	public FilterRegistrationBean<CorsFilter> registrationBean() {
+		FilterRegistrationBean<CorsFilter> filterFilterRegistrationBean = new FilterRegistrationBean<>();
+		UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+
+		CorsConfiguration corsConfiguration = new CorsConfiguration();
+		corsConfiguration.addAllowedOrigin("*");
+		corsConfiguration.addAllowedHeader("*");
+
+		urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+
+		CorsFilter corsFilter = new CorsFilter(urlBasedCorsConfigurationSource);
+
+		filterFilterRegistrationBean.setFilter(corsFilter);
+		filterFilterRegistrationBean.setOrder(0);
+
+		return filterFilterRegistrationBean;
 	}
 
 	@Bean
